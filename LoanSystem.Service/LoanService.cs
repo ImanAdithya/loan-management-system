350d5314ddbe3e457c042e730bec577cfed4d641
adithya <adithyaiman07@gmail.com>
2025-07-25 16:42:21 +0530

    Added loan service class with save loan

---
+++ LoanSystem.Service/LoanService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoanSystem.Entry;
+using LoanSystem.Domain;
+
+namespace LoanSystem.Service
+{
+    public class LoanService
+    {
+        public async Task<bool> InsertLoanApplication(LoanDetails details)
+        {
+            return await new LoanEntry().InsertLoanApplication(details);
+        }
+    }
+}
