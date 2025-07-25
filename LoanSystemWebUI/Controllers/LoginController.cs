117b42a7f4c424e3a951fd3228276e2de38a15b7
adithya <adithyaiman07@gmail.com>
2025-07-25 15:53:57 +0530

    Added  Login Page with UI

---
+++ LoanSystemWebUI/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoanSystemWebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }     
+        
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username == "admin" && password == "123")
+            {
+                return RedirectToAction("Index", "Loan");
+            }
+
+            ViewBag.Error = "Invalid username or password.";
+            return View("Index");
+
+        }
+    }
+}
